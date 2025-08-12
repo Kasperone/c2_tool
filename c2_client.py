55625b358b702f56e0e54c9abf468a3aac1e8445
kali-ninja <kali-ninja@kali-ninja.kali-ninja>
2025-08-12 23:31:48 +0200

    wip c2

--- c2_client.py
+++ c2_client.py
@@ -1,10 +1,17 @@
-import requests
+# Command and Control client Code
 
+from requests import get
+
+# Keep the User-Agent look like a modern browser
 HEADER = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/557.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
     "Content-Type": "application/x-www-form-urlencoded",
     "Accept": "text/plain"
 }
 
-x = requests.get(url='http://localhost:80', headers=HEADER)
+# Set a proxy to match what your target network will be using, if you know it
+# PROXY = None {"https": "proxy.some-site.com:443"}
+PROXY = None
+
+x = get(url='http://localhost:80', headers=HEADER, proxies=PROXY)
 print(x.request.headers)
